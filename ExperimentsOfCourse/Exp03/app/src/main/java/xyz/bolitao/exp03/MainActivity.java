5a6722797f37c859def7b3c793c87a3897af27a0
Boli Tao <tblsyx@outlook.com>
2018-11-17 22:25:05 +0800

    change OS to macOS

--- ExperimentsOfCourse/Exp03/app/src/main/java/xyz/bolitao/exp03/MainActivity.java
+++ ExperimentsOfCourse/Exp03/app/src/main/java/xyz/bolitao/exp03/MainActivity.java
@@ -4,32 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+/**
+ * @author Boli Tao
+ */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Spinner spinner;
+    private Button clickButton;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(
-                this, R.array.spinner, android.R.layout.simple_spinner_item);
+        clickButton = findViewById(R.id.clickButton);
+        imageView = findViewById(R.id.imageView);
+        clickButton.setOnClickListener(this);
+        imageView.setOnClickListener(this);
+        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(arrayAdapter);
-//        spinner.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-
+            case R.id.checkBoxGroup:
+                Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.clickButton:
+                Toast.makeText(getApplicationContext(), "You clicked \"点击\" button.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.imageView:
+                Toast.makeText(getApplicationContext(), "You clicked image.", Toast.LENGTH_SHORT).show();
+                break;
             default:
                 break;
         }
