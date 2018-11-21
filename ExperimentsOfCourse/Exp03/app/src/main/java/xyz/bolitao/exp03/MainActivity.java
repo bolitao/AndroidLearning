c8be4c838afe6ceb93b29f4328a98c998c5b3409
Boli Tao <tblsyx@outlook.com>
2018-11-21 22:22:35 +0800

    change to macOS

--- ExperimentsOfCourse/Exp03/app/src/main/java/xyz/bolitao/exp03/MainActivity.java
+++ ExperimentsOfCourse/Exp03/app/src/main/java/xyz/bolitao/exp03/MainActivity.java
@@ -3,6 +3,7 @@ package xyz.bolitao.exp03;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -21,22 +22,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView imageView;
     private CheckBox reading;
     private CheckBox exercise;
+    private RadioButton male;
+    private RadioButton female;
     private RadioGroup radioGroup;
+    private String[] address = {"湖南", "湖北", "广东"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spinner = findViewById(R.id.spinner);
-        clickButton = findViewById(R.id.clickButton);
-        imageView = findViewById(R.id.imageView);
+//        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
+//        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(arrayAdapter);
+        ArrayAdapter<String> addressAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, address);
+        addressAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(addressAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "你选择的是" + address[position], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+//        clickButton = findViewById(R.id.clickButton);
+//        clickButton.setOnClickListener(this);
+//        imageView = findViewById(R.id.imageView);
+//        imageView.setOnClickListener(this);
         reading = findViewById(R.id.reading);
         exercise = findViewById(R.id.exercise);
-        radioGroup = findViewById(R.id.radioButtonGroup);
-        final RadioButton male = findViewById(R.id.male);
-        final RadioButton female = findViewById(R.id.female);
-        clickButton.setOnClickListener(this);
-        imageView.setOnClickListener(this);
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
         View.OnClickListener onCheckBoxClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -52,19 +71,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         };
         reading.setOnClickListener(onCheckBoxClickListener);
         exercise.setOnClickListener(onCheckBoxClickListener);
+        radioGroup = findViewById(R.id.radioButtonGroup);
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == male.getId()) {
-                    Toast.makeText(MainActivity.this, "你的性别是：" + male.getText(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "的性别是：" + male.getText(), Toast.LENGTH_SHORT).show();
                 } else if (checkedId == female.getId()) {
-                    Toast.makeText(MainActivity.this, "您的性别是：" + female.getText(), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "你的性别是：" + female.getText(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        ArrayAdapter<CharSequence> arrayAdapter = ArrayAdapter.createFromResource(this, R.array.spinner, android.R.layout.simple_spinner_item);
-        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(arrayAdapter);
     }
 
     @Override
@@ -73,12 +90,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.checkBoxGroup:
                 Toast.makeText(getApplicationContext(), "Hello world", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.clickButton:
-                Toast.makeText(getApplicationContext(), "You clicked \"点击\" button.", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.imageView:
-                Toast.makeText(getApplicationContext(), "You clicked image.", Toast.LENGTH_SHORT).show();
-                break;
+//            case R.id.clickButton:
+//                Toast.makeText(getApplicationContext(), "You clicked \"点击\" button.", Toast.LENGTH_SHORT).show();
+//                break;
+//            case R.id.imageView:
+//                Toast.makeText(getApplicationContext(), "You clicked image.", Toast.LENGTH_SHORT).show();
+//                break;
             default:
                 break;
         }
