b2f286f930aa1cb8ff07554f81235caed381eb76
Boli Tao <tblsyx@outlook.com>
2018-12-18 16:17:38 +0800

    Update material demo

---
+++ Mine/MaterialDemo/app/src/main/java/xyz/bolitao/materialdemo/FruitAdapter.java
@@ -0,0 +1,87 @@
+package xyz.bolitao.materialdemo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+    private Context mContext;
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            fruitImage = itemView.findViewById(R.id.fruit_image);
+            fruitName = itemView.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+//    @NonNull
+//    @Override
+//    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+//        if (mContext == null) {
+//            mContext = viewGroup.getContext();
+//        }
+//        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, viewGroup, false);
+//        return new ViewHolder(view);
+//    }
+
+//    @Override
+//    public void onBindViewHolder(@NonNull FruitAdapter.ViewHolder viewHolder, int i) {
+//        // 注意参数中的 ViewHolder 是之前定义的内部类，不是 Adapter 的 ViewHolder
+//        Fruit fruit = mFruitList.get(i);
+//        viewHolder.fruitName.setText(fruit.getName());
+//        Glide.with(mContext).load(fruit.getImageId()).into(viewHolder.fruitImage);
+//    }
+
+//    @Override
+//    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+//        ViewHolder myViewHolder = (ViewHolder) viewHolder;
+//        Fruit fruit = mFruitList.get(i);
+//        myViewHolder.fruitName.setText(fruit.getName());
+//        // 使用 glide 加载图片
+//        Glide.with(mContext).load(fruit.getImageId()).into(myViewHolder.fruitImage);
+//    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        if (mContext == null) {
+            mContext = viewGroup.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Fruit fruit = mFruitList.get(i);
+        viewHolder.fruitName.setText(fruit.getName());
+        // 使用 glide 加载图片
+        Glide.with(mContext).load(fruit.getImageId()).into(viewHolder.fruitImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+}
--- Mine/MaterialDemo/app/src/main/java/xyz/bolitao/materialdemo/MainActivity.java
+++ Mine/MaterialDemo/app/src/main/java/xyz/bolitao/materialdemo/MainActivity.java
@@ -1,18 +1,42 @@
 package xyz.bolitao.materialdemo;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
+    private Fruit[] fruits = {new Fruit("Apple", R.drawable.apple), new Fruit("Banana", R.drawable.banana),
+            new Fruit("Orange", R.drawable.orange), new Fruit("Watermelon", R.drawable.watermelon),
+            new Fruit("Pear", R.drawable.pear), new Fruit("Grape", R.drawable.grape),
+            new Fruit("Pineapple", R.drawable.pineapple), new Fruit("Strawberry", R.drawable.strawberry),
+            new Fruit("Cherry", R.drawable.cherry), new Fruit("Mango", R.drawable.mango)};
+    private List<Fruit> fruitList = new ArrayList<>();
+    private FruitAdapter fruitAdapter;
+    FloatingActionButton floatingActionButton;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
+    /*
+     TODO: 改善 toolbar 图标太大的问题，改进 navigationBar 的菜单按钮
+      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +45,85 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         ActionBar actionBar = getSupportActionBar();
+        NavigationView navigationView = findViewById(R.id.nav_view);
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
+            // 图标会被放大，解决：使用自带工具为不同 DPI 设置图片大小
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
+        navigationView.setCheckedItem(R.id.nav_call);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        floatingActionButton = findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Data deleted", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "Data restored", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
+            }
+        });
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
+        // 自动隐藏或显示 floatingActionBar，如果是 ListView 之类可以在 xml 里直接为 floatingActionBar 中设置 anchor
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                if (newState == recyclerView.SCROLL_STATE_IDLE) {
+                    floatingActionButton.show();
+                }
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                if ((dy != 0) && floatingActionButton.isShown()) {
+                    floatingActionButton.hide();
+                }
+            }
+        });
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+        fruitAdapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(fruitAdapter);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+            }
+        });
+    }
+
+    private void refreshFruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        initFruits();
+                        fruitAdapter.notifyDataSetChanged();
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                });
+            }
+        }).start();
     }
 
     @Override
@@ -41,6 +140,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.backup:
                 Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
+//                Snackbar.make(, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
                 break;
             case R.id.delete:
                 Toast.makeText(this, "You clicked Delete", Toast.LENGTH_SHORT).show();
@@ -55,5 +156,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    private void initFruits() {
+        fruitList.clear();
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
     }
 }
