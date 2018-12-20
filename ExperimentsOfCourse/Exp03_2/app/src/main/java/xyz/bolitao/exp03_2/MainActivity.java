f829506110b8dd5bdd1cfc52585afa14ca90114f
Boli Tao <tblsyx@outlook.com>
2018-12-20 13:31:16 +0800

    Update experiments

--- ExperimentsOfCourse/Exp03_2/app/src/main/java/xyz/bolitao/exp03_2/MainActivity.java
+++ ExperimentsOfCourse/Exp03_2/app/src/main/java/xyz/bolitao/exp03_2/MainActivity.java
@@ -1,13 +1,25 @@
 package xyz.bolitao.exp03_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.linear_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
