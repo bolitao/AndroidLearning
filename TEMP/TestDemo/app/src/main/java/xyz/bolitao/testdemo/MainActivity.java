881bd95525af91a82fd932738af255f9196371a5
Boli Tao <tblsyx@outlook.com>
2018-12-23 02:13:08 +0800

    Update chat demo

---
+++ TEMP/TestDemo/app/src/main/java/xyz/bolitao/testdemo/MainActivity.java
@@ -0,0 +1,40 @@
+package xyz.bolitao.testdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.scaledrone.lib.Member;
+import com.scaledrone.lib.Room;
+import com.scaledrone.lib.RoomListener;
+import com.scaledrone.lib.Scaledrone;
+
+public class MainActivity extends AppCompatActivity implements RoomListener {
+    private String channelID = "CHANNEL_ID_FROM_YOUR_SCALEDRONE_DASHBOARD";
+    private String roomName = "observable-room";
+    private EditText editText;
+    private Scaledrone scaledrone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+    }
+
+    @Override
+    public void onOpen(Room room) {
+        System.out.println("Conneted to room");
+    }
+
+    @Override
+    public void onOpenFailure(Room room, Exception ex) {
+        System.err.println(ex);
+    }
+
+    @Override
+    public void onMessage(Room room, JsonNode message, Member member) {
+    }
+}
