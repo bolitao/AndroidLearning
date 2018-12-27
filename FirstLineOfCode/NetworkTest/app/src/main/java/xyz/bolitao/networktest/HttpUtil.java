2e82dc4121cd309feacff8f4115a4cc06d381aa6
Boli Tao <tblsyx@outlook.com>
2018-12-27 15:59:11 +0800

    UI little fix

--- FirstLineOfCode/NetworkTest/app/src/main/java/xyz/bolitao/networktest/HttpUtil.java
+++ FirstLineOfCode/NetworkTest/app/src/main/java/xyz/bolitao/networktest/HttpUtil.java
@@ -7,26 +7,26 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class HttpUtil {
-    public static String sendHttpRequest(String address) {
-        HttpURLConnection connection = null;
-        try {
-            URL url = new URL(address);
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            connection.setReadTimeout(8000);
-            connection.setConnectTimeout(8000);
-            connection.setDoInput(true);
-            connection.setDoOutput(true);
-            InputStream stream = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            return response.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    public static String sendHttpRequest(String address) {
+//        HttpURLConnection connection = null;
+//        try {
+//            URL url = new URL(address);
+//            connection = (HttpURLConnection) url.openConnection();
+//            connection.setRequestMethod("GET");
+//            connection.setReadTimeout(8000);
+//            connection.setConnectTimeout(8000);
+//            connection.setDoInput(true);
+//            connection.setDoOutput(true);
+//            InputStream stream = connection.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+//            StringBuilder response = new StringBuilder();
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                response.append(line);
+//            }
+//            return response.toString();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
